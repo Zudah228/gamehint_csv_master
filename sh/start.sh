51b2c252840ff461853fb47200daa83763e072e7
Zudah228 <rezel95.hin@gmail.com>
2022-06-30 14:53:27 +0900

    CSVのファイル名を取得できるように変更

---
+++ sh/start.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo " " 
+echo "実行開始..."
+echo "GameHint の CSV ファイルを、_import/ に置いてください。" 
+echo " " 
+
+declare -a filenamelist=()
+i=1
+for naturefilename in _import/*.csv; do
+  filename=${naturefilename:4}
+  if [ "${filename::5}" != "utils" ]; then
+    filenamelist+=(${filename##*/})
+    echo "${i}: ${filename##*/}"
+    let i++
+  fi
+done
+
+echo " " 
+echo "利用する、CSV ファイルの名前を選択してください。"; read key 
+echo "ファイル名が、Challonge トーナメントのタイトルになります。"
+echo " " 
+
+
+node lib/bundle.js ${filenamelist[$key-1]%.*}
+echo " " 
+echo "実行終了"
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,47 @@
 import { Csv } from "./domain/repositories/game_hint/Csv";
 import path from "path";
 import { GamehintParticipant } from "./domain/entities/gamehint/Participant";
-import { tournamentRepository } from "./domain/repositories/challonge/TournamentRepository";
+import {
+  TournamentCreateParam,
+  tournamentRepository,
+} from "./domain/repositories/challonge/TournamentRepository";
+import { getCommandLineArg } from "./utils/CommandLineArg";
 
 async function run() {
+  const csvFileName = getCommandLineArg("csvFileName");
+  if (csvFileName === undefined) {
+    console.error("コマンドライン引数が無効です。");
+    return;
+  }
   const csv = new Csv<GamehintParticipant>(
-    `${path.dirname("")}/_import/game_hint.csv`
+    `${path.dirname("")}/_import/${csvFileName}.csv`
   );
 
   console.log(csv.indices[0].申し込み日時);
+  console.log(csvFileName);
 
   try {
-    const result = await tournamentRepository.create({
+    const createParam: TournamentCreateParam = {
       "tournament[type]": "double elimination",
-      "tournament[name]": "api_test",
-    });
+      "tournament[name]": csvFileName,
+    };
+    console.log("トーナメントを作成します");
+    console.log(
+      `作成するトーナメントの名前：${createParam["tournament[name]"]}`
+    );
+    const result = await tournamentRepository.create(createParam);
+
+    console.log("トーナメントの作成に成功しました！");
+
+    console.log("再取得中...");
+    console.log("");
 
     const tournament = await tournamentRepository.retrieve(
       result.id.toString()
     );
 
-    console.log("トーナメントの作成に成功しました！");
-    console.log(`トーナメントの名前：${tournament.name}`);
+    console.log("再取得に成功しました！");
+    console.log(`再取得したトーナメントの名前：${tournament.name}`);
   } catch (e) {
     console.error(e);
   }
